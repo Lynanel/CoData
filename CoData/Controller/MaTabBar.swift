115904f5e612665865dad6bdd416960d09e6c258
TRAORE Lionel <traore.lionel@icloud.com>
2019-08-13 02:11:33 +0000

    Création de UITabBarController dans notre application

---
+++ CoData/Controller/MaTabBar.swift
@@ -0,0 +1,26 @@
+//
+//  MaTabBar.swift
+//  CoData
+//
+//  Created by TRAORE Lionel on 8/13/19.
+//  Copyright © 2019 TRAORE Lionel. All rights reserved.
+//
+
+import UIKit
+
+class MaTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.tintColor = .darkGray
+        tabBar.unselectedItemTintColor = .lightGray
+        
+        //changer le background color de ma tabbar
+        //tabBar.barTintColor = .yellow
+        
+        //tabbar transparente
+        tabBar.barTintColor = .clear
+        tabBar.backgroundImage = UIImage()
+
+    }
+}
