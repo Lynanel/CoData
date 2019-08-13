d3002d0e67e23cf03ac7e3bead9f8c8ff40c7886
TRAORE Lionel <traore.lionel@icloud.com>
2019-08-13 17:06:12 +0000

    Création de UI AllAricleController

--- CoData/Controller/AllArticlesController.swift
+++ CoData/Controller/AllArticlesController.swift
@@ -8,15 +8,63 @@
 
 import UIKit
 
-class AllArticlesController: UIViewController {
+class AllArticlesController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var liste: Liste!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var isSearching = false
+    var allArticles: [Articles] = []
+    
+    var queryArticle: [Articles] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let nib = UINib(nibName: articleCell, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: articleCell)
+        
+        searchBar.delegate = self
 
-        // Do any additional setup after loading the view.
     }
     
+}
 
+
+extension AllArticlesController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //<#code#>
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+       // <#code#>
+    }
+}
+
+extension AllArticlesController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearching {
+            return queryArticle.count
+        } else {
+            return allArticles.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: articleCell) as? MonArticleCell {
+            if isSearching {
+                cell.setup(queryArticle[indexPath.row])
+            } else {
+                cell.setup(allArticles[indexPath.row])
+            }
+            return cell
+        }
+        return UITableViewCell()
+    }
 }
