6da6e107ffa994f79312f6bdc8be43d0baad5bad
TRAORE Lionel <traore.lionel@icloud.com>
2019-08-13 13:21:36 +0000

    Ajout de UIImagePickerController

--- CoData/Controller/AddArticleController.swift
+++ CoData/Controller/AddArticleController.swift
@@ -13,13 +13,15 @@ class AddArticleController: UIViewController {
     @IBOutlet weak var imageIV: UIView!
     @IBOutlet weak var nameTF: UITextField!
     @IBOutlet weak var shopTF: UITextField!
-    
+    @IBOutlet weak var imageView: UIImageView!
     
     var liste: Liste!
+    var picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        picker.delegate = self
+        picker.allowsEditing = false
         // Do any additional setup after loading the view.
     }
     
@@ -29,9 +31,33 @@ class AddArticleController: UIViewController {
     
     
     @IBAction func pictureGallery(_ sender: Any) {
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
     }
     
     @IBAction func camera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    
+}
+
+
+extension AddArticleController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let or = info[.originalImage] as? UIImage {
+            DispatchQueue.main.async {
+                self.imageView.image = or
+            }
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
